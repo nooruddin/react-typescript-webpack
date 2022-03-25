ed807a90a846d6f636402b829a9b1436af1e95e0
Noor Khorasi <nooruddin.khorasi@gmail.com>
2022-03-25 15:31:31 +0000

    adding styles and asset module loaders

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import "./styles.css";
+import Banner from "./banner.jpeg";
+import Logo from "./logo.svg";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <h1>App</h1>
+      <img src={Banner} alt="banner-image" />
+      <img src={Logo} alt="logo-image" />
+    </>
+  );
 };
 
 export default App;
